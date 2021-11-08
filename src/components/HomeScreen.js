3f47dbc4adaa392d19eb4bcd5a0d463606f67a86
Jordi <jordi8101@gmail.com>
2021-11-08 15:08:59 -0800

    first commit

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,9 @@
+export const HomeScreen = ()=>{
+    return (
+        <div className="home__container">
+            <h1>
+                Hola future Developers
+            </h1>
+        </div>
+    )
+}
